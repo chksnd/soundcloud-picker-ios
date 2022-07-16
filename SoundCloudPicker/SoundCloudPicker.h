45d88c84fc02bc2b885491dca5290a4158947372
Aibek Mazhitov <aibekm@spotify.com>
2022-07-16 14:56:23 +0200

    Initial commit

---
+++ SoundCloudPicker/SoundCloudPicker.h
@@ -0,0 +1,18 @@
+//
+//  SoundCloudPicker.h
+//  SoundCloudPicker
+//
+//  Created by Aibek Mazhitov on 16.07.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SoundCloudPicker.
+FOUNDATION_EXPORT double SoundCloudPickerVersionNumber;
+
+//! Project version string for SoundCloudPicker.
+FOUNDATION_EXPORT const unsigned char SoundCloudPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SoundCloudPicker/PublicHeader.h>
+
+
